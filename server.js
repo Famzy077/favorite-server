0615cd4780fbd7a5c6921e68df67e81caf1f6a4e
Akinola Femi <akinolafemi573@gmail.com>
2025-05-17 20:21:24 +0100

    cors configuration

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const  express =  require('express');
 const authRoutes = require('./routes/authRoutes.js');
-
+const cors = require('cors')
 const app = express();
+app.use(cors("/*"))
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
